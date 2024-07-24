11be50470c6735d5029f9faa3b3d0e1a159317e8
Prajjwaldeep Ghosh <prajjwaldeepghosh.leo@gmail.com>
2024-07-24 20:10:53 +0530

    Logging Aspect

---
+++ src/main/java/demo/BeanConfig.java
@@ -0,0 +1,13 @@
+package demo;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "demo")
+@EnableAspectJAutoProxy
+public class BeanConfig {
+}
---
+++ src/main/java/demo/ShoppingCart.java
@@ -0,0 +1,18 @@
+package demo;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShoppingCart {
+    public void checkout(String Status){
+
+        // Cross-cutting concerns:
+        // Logging
+        // Authentication & Authorization
+        // Sanitize the Data
+
+
+        System.out.println("Checkout Method from ShoppingCart called");
+    }
+}
